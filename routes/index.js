675efb0f94c79575d2d81cf286289cd5e1f0ba34
Thamnoon boonmai <58143420@g.cmru.ac.th>
2019-07-13 16:12:11 +0700

    อัพใหม่

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var contact = require("../config/database")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'NonNi' });
+  var query = "SELECT * FROM positions";
+  contact.query(query, function(error,result){
+    res.json(result)
+  })
 });
 
 router.get('/about', function(req, res, next) {
